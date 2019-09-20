6abb4b61386057b8c593e00ea652f8172a7f419e
Mafinar K <mafinar@gmail.com>
2019-09-20 16:59:42 -0400

    Add poolboy

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule Dgraphex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:grpc, github: "elixir-grpc/grpc"}
+      {:grpc, github: "elixir-grpc/grpc"},
+      {:poolboy, "~> 1.5.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
