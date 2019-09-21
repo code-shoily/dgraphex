a6844159b7f4ae88fa06fb575f56fa55439fff0c
Mafinar K <mafinar@gmail.com>
2019-09-21 03:08:52 -0400

    Add Jason library

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule Dgraphex.MixProject do
   defp deps do
     [
       {:grpc, github: "elixir-grpc/grpc"},
-      {:poolboy, "~> 1.5.1"}
+      {:poolboy, "~> 1.5.1"},
+      {:jason, "~> 1.1.2"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
